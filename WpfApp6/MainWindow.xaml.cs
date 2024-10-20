79cefa8773571118580d4fb66cacaea6e0148085
Kam300 <kamilgaripov205@gmail.com>
2024-10-20 20:42:13 +0500

    one

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameTextBox.Text))
+            {
+                MessageBox.Show("Пожалуйста, введите ваше имя.", "Ошибка ввода",
+                                MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show($"Ваше имя: {NameTextBox.Text}", "Информация",
+                                MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = "Сохранить текст"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    writer.Write(TextBoxInput.Text);
+                }
+
+                MessageBox.Show("Файл успешно сохранён!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void FontSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBoxInput.FontSize = e.NewValue;
+        }
+
+        private void OpenImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openFile.ShowDialog() == true)
+            {
+                string fileName = openFile.FileName;
+                img1.Source = new BitmapImage(new Uri(fileName));
+            }
+        }
+    }
+}
